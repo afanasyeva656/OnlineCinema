b974270b6ef9c074a871ee221e2079bb47a029b4
asya afanasyeva <afanasyeva656@gmail.com>
2021-10-29 16:19:07 +0200

    Optimized imports

--- app/src/main/java/com/afanasyeva656/onlinecinema/features/movies_list_screen/ui/MoviesListFragment.kt
+++ app/src/main/java/com/afanasyeva656/onlinecinema/features/movies_list_screen/ui/MoviesListFragment.kt
@@ -1,7 +1,6 @@
 package com.afanasyeva656.onlinecinema.features.movies_list_screen.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup

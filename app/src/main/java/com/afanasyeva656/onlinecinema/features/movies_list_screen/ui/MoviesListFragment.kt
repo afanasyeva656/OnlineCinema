405f9148cced67252c315d4322965dcc325fee51
asya afanasyeva <afanasyeva656@gmail.com>
2021-10-31 20:41:03 +0200

    Added binding into MoviesListFragment.kt and RecyclerView

--- app/src/main/java/com/afanasyeva656/onlinecinema/features/movies_list_screen/ui/MoviesListFragment.kt
+++ app/src/main/java/com/afanasyeva656/onlinecinema/features/movies_list_screen/ui/MoviesListFragment.kt
@@ -4,19 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.afanasyeva656.onlinecinema.R
 import com.afanasyeva656.onlinecinema.databinding.FragmentMoviesListBinding
+import com.afanasyeva656.onlinecinema.features.movies_list_screen.ui.adapter.MoviesAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MoviesListFragment: Fragment() {
+class MoviesListFragment : Fragment() {
     private val viewModel by viewModel<MoviesListViewModel>()
+    private val adapter by lazy { MoviesAdapter(listOf()) }
     private lateinit var binding: FragmentMoviesListBinding
 
     companion object {
-        fun newInstance() : MoviesListFragment = MoviesListFragment()
+        fun newInstance(): MoviesListFragment = MoviesListFragment()
     }
 
     override fun onCreateView(
@@ -24,16 +27,21 @@ class MoviesListFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_movies_list, container, false)
+        binding = FragmentMoviesListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentMoviesListBinding.inflate(layoutInflater)
+        val recyclerView = binding.rvMovies
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
         viewModel.viewState.observe(viewLifecycleOwner, Observer(::render))
     }
 
     private fun render(state: ViewState) {
-        binding.tvTitle.text = state.moviesList.toString()
+        adapter.updateMovies(state.moviesList)
     }
 }

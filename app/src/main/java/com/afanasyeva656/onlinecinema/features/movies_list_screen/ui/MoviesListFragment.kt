11f0622c7110ec6d899c9ec19f721ff0056dee86
asya afanasyeva <afanasyeva656@gmail.com>
2021-12-02 22:19:34 +0200

    Refactoring :)

--- app/src/main/java/com/afanasyeva656/onlinecinema/features/movies_list_screen/ui/MoviesListFragment.kt
+++ app/src/main/java/com/afanasyeva656/onlinecinema/features/movies_list_screen/ui/MoviesListFragment.kt
@@ -1,9 +1,7 @@
 package com.afanasyeva656.onlinecinema.features.movies_list_screen.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager

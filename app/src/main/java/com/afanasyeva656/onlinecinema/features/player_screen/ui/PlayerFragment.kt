b60bfa59adeb630862d208a3d6ecae3b37959af9
Anastasia Afanasyeva <anastasia_v_a@icloud.com>
2021-12-14 19:05:55 +0200

    Added Player Listener

--- app/src/main/java/com/afanasyeva656/onlinecinema/features/player_screen/ui/PlayerFragment.kt
+++ app/src/main/java/com/afanasyeva656/onlinecinema/features/player_screen/ui/PlayerFragment.kt
@@ -6,19 +6,26 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.afanasyeva656.onlinecinema.R
+import com.afanasyeva656.onlinecinema.databinding.FragmentMoviesListBinding
 import com.afanasyeva656.onlinecinema.databinding.FragmentPlayerBinding
 import com.afanasyeva656.onlinecinema.features.about_movie_screen.ui.AboutMovieFragment
 import com.afanasyeva656.onlinecinema.features.movies_list_screen.domain.model.MovieDomainModel
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.util.Util
 import org.koin.android.ext.android.get
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
 
-class PlayerFragment : Fragment() {
-    private lateinit var binding: FragmentPlayerBinding
+class PlayerFragment : Fragment(R.layout.fragment_player) {
+    private val binding: FragmentPlayerBinding by viewBinding(
+        FragmentPlayerBinding::bind
+    )
     private var player: ExoPlayer? = null
 
     private var playWhenReady = true
@@ -31,15 +38,6 @@ class PlayerFragment : Fragment() {
         fun newInstance(movieUrl: String): PlayerFragment = PlayerFragment().apply {
             arguments = bundleOf(MOVIE_URL_KEY to movieUrl)
         }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentPlayerBinding.inflate(inflater, container, false)
-        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -84,8 +82,7 @@ class PlayerFragment : Fragment() {
 
     private fun initializePlayer() {
         player = get()
-        val movieUrl: String = requireArguments().getString(MOVIE_URL_KEY)
-            ?: "http://techslides.com/demos/sample-videos/small.mp4"
+        val movieUrl: String = requireArguments().getString(MOVIE_URL_KEY)!!
 
         player?.let {
             binding.playerView.player = it
@@ -93,6 +90,18 @@ class PlayerFragment : Fragment() {
             it.playWhenReady = playWhenReady
             it.seekTo(currentWindow, playbackPosition)
             it.prepare()
+            it.addListener(object : Player.Listener {
+                override fun onPlayerStateChanged(playWhenReady: Boolean,playbackState: Int) {
+                    when (playbackState) {
+                        Player.STATE_BUFFERING -> {
+                            binding.progressBar.isVisible = true
+                        }
+                        Player.STATE_READY -> {
+                            binding.progressBar.isVisible = false
+                        }
+                    }
+                }
+            })
         }
     }
 
@@ -118,12 +127,9 @@ class PlayerFragment : Fragment() {
             // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
             // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
             requireActivity().window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                    // Set the content to appear under the system bars so that the
-                    // content doesn't resize when the system bars hide and show.
                     or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    // Hide the nav bar and status bar
                     or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                     or View.SYSTEM_UI_FLAG_FULLSCREEN)
         }
